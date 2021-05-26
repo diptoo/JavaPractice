742bf18272ec233fecd5993b1632a80b31addb91
dipto <dipto33120@gmail.com>
2021-05-27 02:48:04 +0600

    anonymous inner class example by neso

--- src/App.java
+++ src/App.java
@@ -8,20 +8,15 @@ import TestClassFolder.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        
-        Account[] accounts = new Account[2];//array declare
+        GeoObject obj = new GeoObject(){ // anonymous inner class
+            @Override
+            public double getArea() {
+                // TODO Auto-generated method stub
+                return 0;
+            }
+        };
 
-        accounts[0] = new SavingAccount(1, 10.0, new Client(1, "c1", "123"), 60000);
-        accounts[1] = new CurrentAccount(2, 20, new Client(2, "C2", "345"), 45000);
-
-        accounts[0].deposit(500);
-        accounts[0].withdraw(200);
-
-        accounts[1].deposit(300);
-        accounts[1].withdraw(100);
-
-        System.out.println(Arrays.toString(accounts));
+        System.out.println(obj.getArea());
     }
 }
 
